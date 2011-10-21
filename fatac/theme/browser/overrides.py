b1c2d1cf9a8a30bd1f72dee3aa63df5bfab67148
Laura Pérez Mayos <laura.perez-mayos@upcnet.es>
2011-10-21 12:36:29 +0200

    varis: estils, imatges, viewlets, idiomes...

--- fatac/theme/browser/overrides.py
+++ fatac/theme/browser/overrides.py
@@ -1,6 +1,23 @@
 from plone.app.layout.viewlets.common import PersonalBarViewlet
+from plone.app.i18n.locales.browser.selector import LanguageSelector
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
 class PersonalBarViewletFatac(PersonalBarViewlet):
     index = ViewPageTemplateFile('personal_bar.pt')
+
+
+class LanguageSelectorFatac(LanguageSelector):
+    render = ViewPageTemplateFile('languageselector.pt')
+
+    def retIdiomes(self):
+        """
+        """
+        altres_idiomes = []
+        for lang in self.languages():
+            dades = {'code': lang['code'], 'name': lang['name'], 'shortname': lang['name'][0:3].lower()}
+            if lang['selected']:
+                idioma_actual = dades
+            else:
+                altres_idiomes.append(dades)
+        return {'idioma_actual': idioma_actual, 'altres_idiomes': altres_idiomes}
--- fatac/theme/browser/viewlets.py
+++ fatac/theme/browser/viewlets.py
@@ -1,2 +1,18 @@
-#from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-#from plone.app.layout.viewlets.common import ViewletBase
+# -*- coding: utf-8 -*-
+from plone.app.layout.viewlets.common import ViewletBase
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+class cercador(ViewletBase):
+    render = ViewPageTemplateFile('cercador.pt')
+
+
+class onestem(ViewletBase):
+    render = ViewPageTemplateFile('onestem.pt')
+
+
+class opcions(ViewletBase):
+    render = ViewPageTemplateFile('opcions.pt')
+
+class documentsRelacionats(ViewletBase):
+    render = ViewPageTemplateFile('documents_relacionats.pt')
