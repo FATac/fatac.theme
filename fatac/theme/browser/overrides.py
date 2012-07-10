4092cec85bd9fd495973319e093a0d769eff2863
Laura Pérez Mayos <lpmayos@gmail.com>
2012-07-10 13:03:58 +0200

    esborrar funció old

--- fatac/theme/browser/overrides.py
+++ fatac/theme/browser/overrides.py
@@ -16,19 +16,6 @@ class LanguageSelectorFatac(LanguageSelector):
     """
     render = ViewPageTemplateFile('templates/languageselector.pt')
 
-    def retIdiomesOld(self):
-        """ retorna un diccionari amb el codi i el nom de l'idioma actiu i dels
-        idiomes disponibles
-        """
-        altres_idiomes = []
-        for lang in self.languages():
-            dades = {'code': lang['code'], 'name': lang['name']}
-            if lang['selected']:
-                idioma_actual = dades
-            else:
-                altres_idiomes.append(dades)
-        return {'idioma_actual': idioma_actual, 'altres_idiomes': altres_idiomes}
-
     def retIdiomes(self):
         """ retorna un diccionari amb la url, el codi i el nom de l'idioma actiu i dels
         idiomes disponibles
