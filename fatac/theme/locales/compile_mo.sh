e4a46b6dbb939b54bc5780d438524d6197e57f0e
Corina Riba <corina.riba@upcnet.es>
2012-07-12 11:57:50 +0200

    Traduccions i script generació pot

--- fatac/theme/locales/compile_mo.sh
+++ fatac/theme/locales/compile_mo.sh
@@ -6,3 +6,4 @@ msgfmt -v -o en/LC_MESSAGES/fatac.theme.mo  en/LC_MESSAGES/fatac.theme.po
 msgfmt -v -o ca/LC_MESSAGES/plone.mo  ca/LC_MESSAGES/plone.po
 msgfmt -v -o es/LC_MESSAGES/plone.mo  es/LC_MESSAGES/plone.po
 msgfmt -v -o en/LC_MESSAGES/plone.mo  en/LC_MESSAGES/plone.po
+
