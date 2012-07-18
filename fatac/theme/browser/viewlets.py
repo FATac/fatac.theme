9c9bf09b6f132461e003fcb0b7dcd9d4d6b959c2
Laura Pérez Mayos <lpmayos@gmail.com>
2012-07-18 12:38:42 +0200

    links a las ocndiciones de uso desde el formulario de registro + traspaso funciones ocmunes a utils

--- fatac/theme/browser/viewlets.py
+++ fatac/theme/browser/viewlets.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from fatac.theme.browser.utils import utilitats
 
 
 class cercador(ViewletBase):
     render = ViewPageTemplateFile('templates/cercador.pt')
 
 
-class footer(ViewletBase):
+class footer(ViewletBase, utilitats):
     """
     """
     render = ViewPageTemplateFile('templates/footer.pt')
@@ -15,73 +16,4 @@ class footer(ViewletBase):
     #def __init__(self, context, request, view, manager=None):
     #    super(footer, self).__init__(context, request, view, manager)
 
-    def titolPortal(self):
-        """ retorna el títol del portal
-        """
 
-        return self.portal_state.navigation_root_title()
-
-    def isRoot(self):
-        """El viewlet mostra contingut sengons si estem a la pagina principal"""
-        # Recuperem la url sense la vista
-        # (self.request.getURL() ens retorna la vista del document) aixi que fem
-        # servir other['ACTUAL_URL']
-        current = self.request.other['ACTUAL_URL']
-        # Aquesta url pot contenir una / al final, però self.context.portal_url
-        # mai en porta, treiem la / de current abans de comparar.
-        if current[-1] == '/':
-            current = current[:-1]
-        return self.context.portal_url() == current
-
-    def retLinkGeneric(self, id, tipus):
-        """ retorna un diccionari amb el títol, la descripció i la url de
-        l'objecte de tipus 'tipus' amb id 'id'
-        """
-        brain = self.context.portal_catalog.searchResults(portal_type=tipus, id=id)[:1]
-        for i in brain:
-            trad = i.getObject().getTranslation()
-            if trad:
-                return {'titol': trad.Title(), 'descripcio': trad.Description(), 'enllac': trad.absolute_url()}
-        return None
-
-    def linkequip(self):
-        """ retorna un diccionari amb el títol, la descripció i la url del
-        document amb id equip, en l'idioma actual
-        """
-        return self.retLinkGeneric('equip', 'Document')
-
-    def linkajuda(self):
-        """ retorna un diccionari amb el títol, la descripció i la url del
-        document amb id ajuda, en l'idioma actual
-        """
-        return self.retLinkGeneric('ajuda', 'Document')
-
-    def linkcontacte(self):
-        """ retorna un diccionari amb el títol, la descripció i la url de la
-        carpeta amb id contacte, en l'idioma actual
-        """
-        return self.retLinkGeneric('contacte', 'Folder')
-
-    def linkabout(self):
-        """ retorna un diccionari amb el títol, la descripció i la url de la
-        carpeta amb id contacte, en l'idioma actual
-        """
-        return self.retLinkGeneric('sobre-aquest-web', 'Document')
-
-    def linkcomfunciona(self):
-        """ retorna un diccionari amb el títol, la descripció i la url de la
-        carpeta amb id contacte, en l'idioma actual
-        """
-        return self.retLinkGeneric('com-funciona', 'Document')
-
-    def linkcredits(self):
-        """ retorna un diccionari amb el títol, la descripció i la url de la
-        carpeta amb id contacte, en l'idioma actual
-        """
-        return self.retLinkGeneric('credits', 'Document')
-
-    def linkavislegal(self):
-        """ retorna un diccionari amb el títol, la descripció i la url de la
-        carpeta amb id contacte, en l'idioma actual
-        """
-        return self.retLinkGeneric('avis-legal', 'Document')
